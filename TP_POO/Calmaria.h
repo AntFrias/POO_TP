9d333dfce5b4ba25be103e66052ecbbb587bc525
Unknown <a21230573@isec.pt>
2018-12-15 13:15:14 +0000

    Base para Tratar os Eventos feita;
    Codigo pronto para começar a fazer regras dos eventos

--- TP_POO/Calmaria.h
+++ TP_POO/Calmaria.h
@@ -3,10 +3,15 @@
 
 #include "Eventos.h"
 
+#define EVENTO_CALMARIA 3
+
 class Calmaria : public Eventos
 {
 public:
-	Calmaria(Mundo *mundo) : Eventos(mundo, TTL_CALMARIA) {};
+	Calmaria(Mundo *mundo) : Eventos(mundo, EVENTO_CALMARIA) {};
+	virtual bool ValidaEventoCalmaria();
+	virtual void TrataEventoCalmaria();
+	virtual void TrataEventoMotim() {};
 	~Calmaria();
 };
 #endif // !CALMARIA_H
